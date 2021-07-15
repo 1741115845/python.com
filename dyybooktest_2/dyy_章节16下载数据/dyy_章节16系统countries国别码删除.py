affbc286abf0e2b969dd90b368ceff896ed8b452
dyy <dengyingying.com>
2021-07-15 23:34:11 +0800

    commit all

---
+++ dyybooktest_2/dyy_章节16下载数据/dyy_章节16系统countries国别码删除.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+# Author:dyy
+from pygal_maps_world.i18n import COUNTRIES
+def get_country_code(country_name):
+	for code,name in COUNTRIES.items():
+		if name==country_name:
+			return code
+	return None
+print("测试删除")
+print("这个时master分支")
+
